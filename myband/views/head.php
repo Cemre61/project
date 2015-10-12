cee750d3f59a1ad490880597c0d0f947d49906d0
Cemre61 <c.sari61@hotmail.com>
2015-10-12 11:54:27 +0200

    Update head.php

--- myband/views/head.php
+++ myband/views/head.php
@@ -2,7 +2,7 @@
 
     <head>
         <meta charset="utf-8">
-        <title><?php echo(TITLE);?></title>
+        <title>Real Madrid</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
