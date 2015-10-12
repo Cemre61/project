e24eed19cb83ce7eb5f69a4e1f0635bb78a00eec
Cemre61 <c.sari61@hotmail.com>
2015-10-12 15:19:22 +0200

    updated

--- myband/logic/about.php
+++ myband/logic/about.php
@@ -1,7 +1,8 @@
 <div class="background">
-    pagina moet nog geupdate worden<br>
-    
-    ...<br>
-    ...<br>
-    ...
+     <div id="videos">
+    <h1>About us</h1>    
+         <iframe width="1080" height="480" src="https://www.youtube.com/embed/pp82afzX4Fg?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe></div>
+<p>In the first few years of this new decade three trophies were added to the cabinet by José Mourinho's Real Madrid. The most notable was the 2011-12 Liga title, which they won with a record-breaking 100 points, the highest score achieved in the history of the championship at that stage, and 121 goals. They also beat Barcelona to clinch a Copa del Rey and a Supercopa de España.
+
+In June 2013, Carlo Ancelotti arrived at Real Madrid. The Italian won four trophies with the Whites, the first coming in the shape of Real Madrid's nineteenth Copa del Rey, after beating Barcelona 1-2 in the final. On 24 May 2014, Madrid lifted the European Cup once again, claiming La Décima with a 4-1 victory over Atlético in Lisbon.
 </div>
--- myband/views/search.tpl
+++ myband/views/search.tpl
@@ -1,5 +1,5 @@
 <div class="search">
 <form method="post" action="?action=search">
-    <input type="text" name="search_string" value="Search for an article">
+    <input type="text" name="search_string" value=" Search for an article">
     <input type="submit" value="Search">
     </form></div>
