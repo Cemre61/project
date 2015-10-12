3ca4492a7e293435e9d7de6b4a759c4776f73869
Cemre61 <c.sari61@hotmail.com>
2015-10-12 11:35:08 +0200

    Update search_articles.php

--- myband/models/search_articles.php
+++ myband/models/search_articles.php
@@ -6,4 +6,8 @@ $sql = "SELECT * FROM newsarticles WHERE title LIKE '%$search_string%'";
 
 $search_result = $mysqli->query($sql);
 
+$search_result = $mysqli->query($sql);
+
+$search_result = resultToArray($search_result);
+
 ?>
